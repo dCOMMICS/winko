8792745e8188b4ac88ae98763f48ec0f6af7762e
dCOMMICS <sanwurld@gmail.com>
2023-10-25 17:15:50 +0300

    ✌

--- winko-website/app/page.tsx
+++ winko-website/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     
-    <div className="h1">
+    <div className="font-bold">
       <h1>
         Welcome
       </h1>
